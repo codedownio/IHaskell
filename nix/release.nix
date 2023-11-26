fe7b97b3bde804e4620ec1d48ff8caecf0dcbdb2
Tom McLaughlin <tom@codedown.io>
2023-11-26 03:48:30 -0700

    Simplifying release.nix

--- nix/release.nix
+++ nix/release.nix
@@ -15,6 +15,7 @@ let
     map
       (display: { name = "ihaskell-${display}-" + compiler; value = self.callCabal2nix display "${ihaskell-src}/ihaskell-display/ihaskell-${display}" {}; })
       [ "aeson" "blaze" "charts" "diagrams" "gnuplot" "graphviz" "hatex" "juicypixels" "magic" "plot" "rlangqq" "static-canvas" "widgets" ]);
+
   haskellPackages = nixpkgs.haskell.packages."${compiler}".override (old: {
     overrides = nixpkgs.lib.composeExtensions (old.overrides or (_: _: {})) ihaskellOverlay;
   });
@@ -41,79 +42,67 @@ let
   # statically linking against haskell libs reduces closure size at the expense
   # of startup/reload time, so we make it configurable
   ihaskellExe = if staticExecutable
-    then nixpkgs.haskell.lib.justStaticExecutables haskellPackages.ihaskell
-    else nixpkgs.haskell.lib.enableSharedExecutables haskellPackages.ihaskell;
+                then nixpkgs.haskell.lib.justStaticExecutables haskellPackages.ihaskell
+                else nixpkgs.haskell.lib.enableSharedExecutables haskellPackages.ihaskell;
 
   ihaskellEnv = haskellPackages.ghcWithPackages packages;
 
-  ihaskellGhcLibFunc = exe: env: nixpkgs.writeShellScriptBin "ihaskell" ''
-    ${exe}/bin/ihaskell -l $(${env}/bin/ghc --print-libdir) "$@"
-  '';
-
-  ihaskellKernelFileFunc = ihaskellGhcLib: rtsopts: {
-    display_name = "Haskell";
-    argv = [
-      "${ihaskellGhcLib}/bin/ihaskell"
-      "kernel"
-      "{connection_file}"
-      "+RTS"
-    ] ++ (nixpkgs.lib.splitString " " rtsopts) ++ [
-      "-RTS"
-    ];
-    language = "haskell";
-  };
-
-  ihaskellKernelSpecFunc = ihaskellKernelFile: nixpkgs.runCommand "ihaskell-kernel" {} ''
-    export kerneldir=$out/kernels/haskell
-    mkdir -p $kerneldir
-    cp ${../html}/* $kerneldir
-    echo '${builtins.toJSON ihaskellKernelFile}' > $kerneldir/kernel.json
-  '';
+  ihaskellKernelSpecFunc = ihaskellGhcLib: rtsopts:
+    let
+      kernelFile = {
+        display_name = "Haskell";
+        argv = [
+          "${ihaskellGhcLib}/bin/ihaskell"
+          "kernel"
+          "{connection_file}"
+          "+RTS"
+        ] ++ (nixpkgs.lib.splitString " " rtsopts) ++ [
+          "-RTS"
+        ];
+        language = "haskell";
+      };
+    in
+      nixpkgs.runCommand "ihaskell-kernel" {} ''
+        export kerneldir=$out/kernels/haskell
+        mkdir -p $kerneldir
+        cp ${../html}/* $kerneldir
+        echo '${builtins.toJSON kernelFile}' > $kerneldir/kernel.json
+      '';
 
   ihaskellLabextension = nixpkgs.runCommand "ihaskell-labextension" {} ''
     mkdir -p $out/labextensions/
     ln -s ${../jupyterlab-ihaskell/labextension} $out/labextensions/jupyterlab-ihaskell
   '';
 
-  ihaskellDataDirFunc = ihaskellKernelSpec: ihaskellLabextension: nixpkgs.buildEnv {
-    name = "ihaskell-data-dir-" + compiler;
-    paths = [ ihaskellKernelSpec ihaskellLabextension ];
-  };
+  ihaskellGhcLib = nixpkgs.writeShellScriptBin "ihaskell" ''
+    ${ihaskellEnv}/bin/ihaskell -l $(${ihaskellEnv}/bin/ghc --print-libdir) "$@"
+  '';
 
-  ihaskellBuildEnvFunc = { ihaskellEnv, systemPackages, ihaskellDataDir }: nixpkgs.buildEnv {
-    name = "ihaskell-with-packages-" + compiler;
-    nativeBuildInputs = [ nixpkgs.makeWrapper ];
-    paths = [ ihaskellEnv jupyterlab ];
-    postBuild = ''
-      for prg in $out/bin"/"*;do
-        if [[ -f $prg && -x $prg ]]; then
-          wrapProgram $prg \
-            --prefix PATH : "${nixpkgs.lib.makeBinPath ([ihaskellEnv] ++ (systemPackages nixpkgs))}" \
-            --prefix JUPYTER_PATH : "${ihaskellDataDir}"
-        fi
-      done
-    '';
-    passthru = {
-      inherit haskellPackages;
-      inherit ihaskellExe;
-      inherit ihaskellEnv;
-      inherit ihaskellOverlay;
-      inherit ihaskellLabextension;
-      inherit ihaskellGhcLibFunc;
-      inherit ihaskellKernelFileFunc;
-      inherit ihaskellKernelSpecFunc;
-      inherit ihaskellDataDirFunc;
-      inherit ihaskellBuildEnvFunc;
-    };
+  ihaskellDataDir = nixpkgs.buildEnv {
+    name = "ihaskell-data-dir-" + compiler;
+    paths = [
+      (ihaskellKernelSpecFunc ihaskellGhcLib rtsopts)
+      ihaskellLabextension
+    ];
   };
 
 in
 
-ihaskellBuildEnvFunc {
-  inherit ihaskellEnv systemPackages;
-  ihaskellDataDir = let
-    ihaskellGhcLib = ihaskellGhcLibFunc ihaskellExe ihaskellEnv;
-    ihaskellKernelFile = ihaskellKernelFileFunc ihaskellGhcLib rtsopts;
-    ihaskellKernelSpec = ihaskellKernelSpecFunc ihaskellKernelFile;
-  in ihaskellDataDirFunc ihaskellKernelSpec ihaskellLabextension;
+nixpkgs.buildEnv {
+  name = "ihaskell-with-packages-" + compiler;
+  nativeBuildInputs = [ nixpkgs.makeWrapper ];
+  paths = [ ihaskellEnv jupyterlab ];
+  postBuild = ''
+    for prg in $out/bin"/"*;do
+      if [[ -f $prg && -x $prg ]]; then
+        wrapProgram $prg \
+          --prefix PATH : "${nixpkgs.lib.makeBinPath ([ihaskellEnv] ++ (systemPackages nixpkgs))}" \
+          --prefix JUPYTER_PATH : "${ihaskellDataDir}"
+      fi
+    done
+  '';
+
+  passthru = {
+    inherit ihaskellExe;
+  };
 }
